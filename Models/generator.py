84b25ca63c2182d102fa1d569f09124de5219be7
Marco D'Amico <marco.damico@bsc.es>
2020-05-15 12:07:27 +0200

    Workflow extension for generator.py

--- Models/generator.py
+++ Models/generator.py
@@ -13,19 +13,20 @@
 
 import subprocess
 import os
-from random import seed
 from random import randint
+from random import seed
+from random import uniform
 seed(1)
 
 # Cirne base model was fixed so that: 
 # 1) traces are in SWF format, and 
 # 2) jobs are ordered from earlier to later arrival time.
 
-base_model_path = "/home/bsc33/bsc33882/SWF_workloads/m_cirne01/genworkload"
+base_model_path = "cirne/genworkload"
 
 current_dir = os.getcwd()
 data=[]
-single_module_percent = 60
+single_module_percent = 100
 
 
 def delete_tmp_files():
@@ -72,7 +73,7 @@ def create_template_trace(module,num_nodes,num_jobs,arr_pattern,load):
 def label_single_multi_jobs(i):
     global data
     random_value = randint(0,100)
-    if random_value < single_module_percent: 
+    if random_value <= single_module_percent: 
         data[i]=data[i].replace(data[i].split(';')[11], "single-module")
     else: 
         data[i]=data[i].replace(data[i].split(';')[11], "multi-module")
@@ -82,9 +83,9 @@ def add_temp_module_list_field(i):
 
 def edit_modlist(filename,modlist):
     # if edited modlist needs to be written in file only, then:
-    filename.write(data[i].replace(data[i].split(';')[18],modlist))
+    #filename.write(data[i].replace(data[i].split(';')[18],modlist))
     # if data list needs to be changed then:
-    # data[i]=data[i].replace(data[i].split(';')[18],modlist)
+    data[i]=data[i].replace(data[i].split(';')[18],modlist)
     # the latter might be the case if we want to pass swf trace with modlist field to creation of wf and hetero jobs file
     # only change data list if filename is basic flexibility case: modlistfile. 
 
@@ -104,7 +105,7 @@ def edit_modlist(filename,modlist):
 # 1) three traces are in SWF format, and 
 # 2) jobs are ordered from earlier to later arrival time.
 
-base_model_path = "/home/bsc33/bsc33882/SWF_workloads/m_cirne01/genworkload"
+base_model_path = "cirne/genworkload"
 
 num_jobs=100
 arrival_pattern="anl"
@@ -136,12 +137,13 @@ with open(current_dir+"/tmp.concatenated.swf",'w') as outfile:
                 outfile.write(line)
 
 f=(current_dir+"/tmp.concatenated.swf")
-data=file(f).readlines()
+data=open(f).readlines()
 
 # Sort lines by job's arrival time and assign jobid to each line
 # Assign jobid to each line
 # Cut the trace when the last job of the shortest trace has arrived
 empty="-1\n"
+num_jobs *= 3
 
 
 with open(current_dir+"/cirne_base_"+str(num_jobs)+"_"+arrival_pattern+"_load_"+str(load_cm)+"_"+str(load_esb)+"_"+str(load_dam)+".swf",'w') as basefile:
@@ -276,6 +278,54 @@ with open(current_dir+"/cirne_modlist_"+str(num_jobs)+"_"+arrival_pattern+"_load
 # Parameter (module DAG, percent of occurence)
 # How do I create random DAGs based on this information?
 
+#parameters
+            max_wf_jobs = 3
+            perc_wf_jobs = 20
+            is_het = [0] * len(data)
+            #much easier to mange data as a list
+            ldata=[]
+            #extend data format to include real delay, use 17th field for the req_delay
+            for i in range(len(data)):
+                data[i] = data[i].replace('\n',";-1\n")
+                ldata.append(data[i].split(';'))
+            i = 0
+            while i < len(data):
+                job_type = data[i].split(';')[11]
+                if job_type == "single-module":
+                    r1 = randint(0,100)
+                    ncomp = 1
+                    iprec = i
+                    inext = i + 1
+                    #extend SWF with two fields
+                    while r1 <= perc_wf_jobs and ncomp < max_wf_jobs and inext < len(data):
+                        is_het[iprec] = 1
+                        #calculate delay from requested time
+                        req_delay = round(uniform(0.5,0.9) * float(ldata[iprec][8]))
+                        #calculate actual delay from duration
+                        delay = round(uniform(0.5,0.9) * float(ldata[iprec][3]))
+                        if req_delay < delay:
+                            req_delay = delay
+                        #set dependency to precedent job TODO:FIX
+                        #data[inext] = data[inext].replace(
+                        #        data[inext].split(";")[16], data[iprec].split(";")[0])
+                        ldata[inext][16] = ldata[iprec][0]
+                        ldata[inext][17] = str(req_delay)
+                        ldata[iprec][19] = str(delay)+'\n'
+                        
+                        #If hetjob set same arrival time of precedent job (initially also for jobs with deps)
+                        ldata[inext][1] = ldata[iprec][1]
+
+                        r1 = randint(0,100)
+                        ncomp += 1
+                        iprec = inext
+                        inext += 1
+                    #ncomp == 1 means it was not a hetjob
+                    i += ncomp
+            for i in range(len(data)):
+                modlistfile.write(';'.join(ldata[i]))
+
+
+#Loop between single jobs and assign a dependency with the following job. If a dependency is created the following job arrival time is modified to current job arrival time.
 
 
 ##########################################################################################
