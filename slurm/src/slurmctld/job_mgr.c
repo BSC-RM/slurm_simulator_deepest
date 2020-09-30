f3f6f805c6b9c937349e09f6ff29855c318f5ed2
Marco D'Amico <marco.damico91@gmail.com>
2020-09-30 17:07:10 +0200

    test_job_nodes_ready now doesn't check for batch_host existance

--- slurm/src/slurmctld/job_mgr.c
+++ slurm/src/slurmctld/job_mgr.c
@@ -8659,6 +8659,7 @@ extern bool test_job_nodes_ready(struct job_record *job_ptr)
 		if ((select_g_job_ready(job_ptr) & READY_NODE_STATE) == 0)
 			return false;
 	} else if (job_ptr->batch_flag) {
+#ifndef SLURM_SIMULATOR
 		/* Make sure first node is ready to start batch job */
 		struct node_record *node_ptr =
 			find_node_record(job_ptr->batch_host);
@@ -8667,6 +8668,7 @@ extern bool test_job_nodes_ready(struct job_record *job_ptr)
 		    IS_NODE_POWER_UP(node_ptr)) {
 			return false;
 		}
+#endif
 	}
 
 	return true;
