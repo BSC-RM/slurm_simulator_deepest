ad4f705c15a70f9f985470b78941ab59b4f9c78f
Marco D'Amico <marco.damico@bsc.es>
2020-11-19 14:55:02 +0100

    Compiler perfromance optimizations

--- compile_and_install_slurm.sh
+++ compile_and_install_slurm.sh
@@ -18,8 +18,9 @@ mkdir -p "${install_dir}/slurm_varios/log"
 
 export LIBS=-lrt
 #export CFLAGS="-D SLURM_SIMULATOR -D WF_API"
-
-export CFLAGS="-D SLURM_SIMULATOR"
+#new gcc versions
+#CFLAGS="-D SLURM_SIMULATOR -g0 -O3 -D NDEBUG=1 -fno-omit-frame-pointer -fcommon"
+export CFLAGS="-D SLURM_SIMULATOR -g0 -O3 -D NDEBUG=1
 
 cd "${slurm_source_dir}"
 
@@ -36,8 +37,7 @@ echo "Running Configure"
 --sharedstatedir=$install_dir/slurm_varios \
 --mandir=$install_dir/slurm_varios/man \
 --prefix=$install_dir/slurm_programs --sysconfdir=$install_dir/slurm_conf \
---enable-front-end \
---enable-simulator 2> slurm_configure.log
+--enable-front-end --disable-debug 2> slurm_configure.log
 
 echo "Compiling"
 make -j4
